b8b7bd93afbcc73d319bc2323c7e336478dbbddc
rahinsoo <rahinsoo@etik.com>
2026-01-08 10:40:33 +0100

    Creat - backend - mini framework php

---
+++ backend/public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Controller\AppController;
+use Controller\GameApiController;
+use Controller\PingApiController;
+use Core\Cors;
+use Core\Database;
+use Core\Request;
+use Core\Response;
+use Core\Router;
+use Core\Session;
+use Repository\GamesRepository;
+
+session_start();
+require __DIR__ . '/../autoload.php';
+
+$config = require_once __DIR__ . '/../config/db.php';
+
+Cors::handle();
+
+$response = new Response();
+$session = new Session();
+$request = new Request();
+$router = new Router();
+$repository = new GamesRepository(Database::makePdo($config['db']));
+
+$appController = new AppController($response, $repository, $session, $request);
+$pingApiController = new PingApiController();
+$gameApiController = new GameApiController($response, $repository);
+
+$registerRoutes = require __DIR__ . '/../config/routes.php';
+$registerRoutes($router, $appController, $pingApiController, $gameApiController);
+$router->dispatch($request, $response);
+
